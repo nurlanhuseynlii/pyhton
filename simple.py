ba53466f559972d3ab3ebecf4c47ccc436b979bb
Nurlan Huseynli <nurlannhuseynlii@gmail.com>
2024-11-29 16:21:41 +0400

    new

---
+++ simple.py
@@ -0,0 +1,13 @@
+limit = 5000
+store = input('what is store name ')
+price = int(input('enter the your money '))
+
+if price > limit:
+  print("congrulation")
+
+elif price < limit:
+  print("error")
+
+else:
+  print('follow continue')
+  
