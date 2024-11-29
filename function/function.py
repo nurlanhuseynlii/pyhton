5379569a73fda4c4ac58f27bafa5ed6ff85c5d93
Nurlan Huseynli <nurlannhuseynlii@gmail.com>
2024-11-29 18:37:20 +0400

    function

---
+++ function/function.py
@@ -0,0 +1,29 @@
+# def don(x):
+#     print(x**2)
+# don(4)
+
+
+
+maaslar = [1000,2000,3000,4000,5000]
+mySalary = int(input("emter you salary: "))
+
+def salary(x):
+  print(x*20/100 +x)
+salary(mySalary)
+
+
+# for i in maaslar:
+#   print(i)
+
+
+
+# def new_salary(x):
+#   print(x*20/100 + x)
+# new_salary(1)
+
+
+# for i in maaslar:
+#   new_salary(i)
+
+
+# 1000*20/100  + 1000
